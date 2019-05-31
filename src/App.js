b2e5e2a372d0b2123a131a80cc905bad050c1375
Douglas Scriptore <douglass@rpc.com.br>
2019-05-31 18:44:29 -0300

    Criando componente do Player

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import React from 'react'
 import GlobalStyle from './styles/global.js'
 
 import Sidebar from './components/Sidebar'
+import Player from './components/Player'
+
+import { Wrapper, Conteiner } from './styles/components'
 
 const App = () => (
-  <React.Fragment>
+  <Wrapper>
     <GlobalStyle />
-    <Sidebar />
-  </React.Fragment>
+    <Conteiner>
+      <Sidebar />
+    </Conteiner>
+    <Player />
+  </Wrapper>
 )
 
 export default App
