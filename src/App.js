6e635dea311db43a43c72e50ee461bb1bebb9b4c
Douglas Scriptore <douglass@rpc.com.br>
2019-06-03 18:04:20 -0300

    Configurando Reactotron, Redux & Saga com Duck Patterns

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import './config/reactotron'
 
 import GlobalStyle from './styles/global.js'
 
@@ -10,21 +12,29 @@ import Header from './components/Header'
 import { Wrapper, Conteiner, Content } from './styles/components'
 
 import Routes from './routes'
+import store from './store'
 
+console.tron.log('Test')
+
+/**
+ * O Provider ta por fora p/ caso queramos controlar as informações de rota pelo Redux
+ */
 const App = () => (
-  <BrowserRouter>
-    <Wrapper>
-      <GlobalStyle />
-      <Conteiner>
-        <Sidebar />
-        <Content>
-          <Header />
-          <Routes />
-        </Content>
-      </Conteiner>
-      <Player />
-    </Wrapper>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Wrapper>
+        <GlobalStyle />
+        <Conteiner>
+          <Sidebar />
+          <Content>
+            <Header />
+            <Routes />
+          </Content>
+        </Conteiner>
+        <Player />
+      </Wrapper>
+    </BrowserRouter>
+  </Provider>
 )
 
 export default App
