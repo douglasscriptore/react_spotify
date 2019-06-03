2d4c9dbd2bff427d41e00eb59af4691caad44911
Douglas Scriptore <douglass@rpc.com.br>
2019-06-03 11:35:41 -0300

    criação do componente Header

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import GlobalStyle from './styles/global.js'
 
 import Sidebar from './components/Sidebar'
 import Player from './components/Player'
+import Header from './components/Header'
 
-import { Wrapper, Conteiner } from './styles/components'
+import { Wrapper, Conteiner, Content } from './styles/components'
 
 const App = () => (
   <Wrapper>
     <GlobalStyle />
     <Conteiner>
       <Sidebar />
+      <Content>
+        <Header />
+      </Content>
     </Conteiner>
     <Player />
   </Wrapper>
