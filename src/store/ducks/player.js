42ef60f2b69112aad5fc4fe1f3f862daad6d49ee
Douglas Scriptore <douglass@rpc.com.br>
2019-06-06 16:00:34 -0300

    dando funcionalidade ao slider

--- src/store/ducks/player.js
+++ src/store/ducks/player.js
@@ -6,7 +6,9 @@ export const Types = {
   PAUSE: 'player/PAUSE',
   NEXT: 'player/NEXT',
   PREV: 'player/PREV',
-  PLAYING: 'player/PLAYING'
+  PLAYING: 'player/PLAYING',
+  HANDLE_POSITION: 'player/HANDLE_POSITION',
+  SET_POSITION: 'player/SET_POSITION'
 }
 
 const INITIAL_STATE = {
@@ -14,6 +16,7 @@ const INITIAL_STATE = {
   list: [],
   status: Sound.status.PLAYING,
   position: null,
+  positionShown: null,
   duration: null
 }
 
@@ -25,7 +28,8 @@ export default function player (state = INITIAL_STATE, action) {
         ...state,
         currentSong: action.payload.song,
         list: action.payload.list,
-        status: Sound.status.PLAYING
+        status: Sound.status.PLAYING,
+        position: 0
       }
     case Types.PLAY:
       return {
@@ -43,7 +47,12 @@ export default function player (state = INITIAL_STATE, action) {
       )
       const prev = state.list[currentIndex - 1]
       if (prev) {
-        return { ...state, currentSong: prev, status: Sound.status.PLAYING }
+        return {
+          ...state,
+          currentSong: prev,
+          status: Sound.status.PLAYING,
+          position: 0
+        }
       }
 
       return {
@@ -56,7 +65,12 @@ export default function player (state = INITIAL_STATE, action) {
       )
       const next = state.list[currentIndex + 1]
       if (next) {
-        return { ...state, currentSong: next, status: Sound.status.PLAYING }
+        return {
+          ...state,
+          currentSong: next,
+          status: Sound.status.PLAYING,
+          position: 0
+        }
       }
 
       return {
@@ -65,6 +79,17 @@ export default function player (state = INITIAL_STATE, action) {
     }
     case Types.PLAYING:
       return { ...state, ...action.payload }
+    case Types.HANDLE_POSITION:
+      return {
+        ...state,
+        positionShown: state.duration * action.payload.percent
+      }
+    case Types.SET_POSITION:
+      return {
+        ...state,
+        position: state.duration * action.payload.percent,
+        positionShown: null
+      }
     default:
       return state
   }
@@ -80,5 +105,13 @@ export const Creators = {
   playing: ({ position, duration }) => ({
     type: Types.PLAYING,
     payload: { position, duration }
+  }),
+  handlePosition: percent => ({
+    type: Types.HANDLE_POSITION,
+    payload: { percent }
+  }),
+  setPosition: percent => ({
+    type: Types.SET_POSITION,
+    payload: { percent }
   })
 }
